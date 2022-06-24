af123b695cffb5b89e165fd6a830d4bfab9ef793
NadiaPia <206i417@gmail.com>
2022-06-24 17:26:03 +0000

    urls_new.ejs ans generateRandomString function added

--- express_server.js
+++ express_server.js
@@ -7,6 +7,22 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString(n) {
+  let randomString = '';
+  let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for ( let i = 0; i < n; i++ ) {
+    const index = Math.floor(Math.random() * characters.length)    
+    randomString += characters[index]
+ }
+ return randomString;
+
+}
+console.log(generateRandomString(6))
+
 app.set ("view engine", "ejs"); //This tells the Express app to use EJS as its templating engine.
 
 app.get("/", (req, res) => {
@@ -16,6 +32,15 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/:shortURL", (req, res) => {
