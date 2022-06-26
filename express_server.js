71e9b3d5026a14aa9e83fc2fbdc2d99ddb99cf83
NadiaPia <206i417@gmail.com>
2022-06-26 18:23:19 +0000

    /u/:shortURLadded

--- express_server.js
+++ express_server.js
@@ -10,6 +10,34 @@ const urlDatabase = {
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+
+
+app.set ("view engine", "ejs"); //This tells the Express app to use EJS as its templating engine.
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+  
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  const shortURL = generateRandomString(6);
+  res.redirect(`/urls/${shortURL}`)
+  //res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[shortURL] = req.body["longURL"];
+  //console.log(urlDatabase)
+
+});
+
 function generateRandomString(n) {
   let randomString = '';
   let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -23,32 +51,21 @@ function generateRandomString(n) {
 }
 console.log(generateRandomString(6))
 
-app.set ("view engine", "ejs"); //This tells the Express app to use EJS as its templating engine.
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars);
-});
-
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
-});
 
 app.get("/urls/:shortURL", (req, res) => {
   
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
-  console.log(req)
+  //console.log(req)
   res.render("urls_show", templateVars);
 });
+
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  console.log(longURL)
+  res.redirect(longURL);
+});
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
